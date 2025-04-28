18ab1f4ff8122f6ac75f625c1d79785d04119c1b
ThorGuy <32145412+ThorGuy@users.noreply.github.com>
2025-04-28 17:28:21 -0400

    Custom config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,42 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-lua/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup()
+    end,
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {}
+    end,
+  },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.animate').setup {
+        cursor = {
+          enable = false,
+        },
+        scroll = {
+          timing = function(_, n)
+            return 20 / n
+          end,
+        },
+      }
+    end,
+  },
+  {
+    'alec-gibson/nvim-tetris',
+  },
+}
